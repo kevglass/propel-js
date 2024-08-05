3f3144b35ad9a17ede99601f365b0ed14aa890f1
Coke And Code <kevin@rune.ai>
2024-08-05 20:55:20 +0100

    Improve joints sample

--- examples/src/examples/Joints.ts
+++ examples/src/examples/Joints.ts
@@ -7,9 +7,9 @@ export function jointsInit(): physics.World {
     for (let i=0;i<3;i++) {
         const rect = physics.createRectangle(world, { x: 150 + (i*70), y: 50 }, 30, 30, 0, 0, 0.5);
         physics.addBody(world, rect);
-        const circle = physics.createCircle(world, { x: 150 + (i*70), y: 250 }, 20, 1, 0, 0.5);
+        const circle = physics.createCircle(world, { x: 150 + (i*70), y: 250 }, 20, 1, 0, 1);
         physics.addBody(world, circle);
-        const joint = physics.createJoint(world, rect, circle, 0.5, 0.5);
+        physics.createJoint(world, rect, circle, 1, 0);
 
         if (i === 0) {
             (circle as physics.DynamicRigidBody).velocity.x = -150;
