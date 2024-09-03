cc1fe425b78c6a6fe36ab9324c1f654e48e20fe4
Coke And Code <kevin@rune.ai>
2024-09-03 20:30:03 +0100

    Update marble demo

--- examples/examples-dist/examples/Marble.js
+++ examples/examples-dist/examples/Marble.js
@@ -4,9 +4,9 @@ export function marbleInit() {
     const world = physics.createWorld();
     world.damp = 0.99;
     world.angularDamp = 0.95;
-    const marble1 = physics.createCircle(world, { x: 225, y: -100 }, 20, 2, 0.3, 0.3);
+    const marble1 = physics.createCircle(world, { x: 245, y: -200 }, 20, 2, 0.3, 0.5);
     physics.addBody(world, marble1);
-    const marble2 = physics.createCircle(world, { x: 275, y: -200 }, 20, 2, 0.3, 0.3);
+    const marble2 = physics.createCircle(world, { x: 275, y: -300 }, 20, 2, 0.3, 0.3);
     physics.addBody(world, marble2);
     const slope1 = physics.createRectangle(world, { x: 200, y: 200 }, 200, 10, 0, 0.3, 0.3);
     physics.rotateBody(slope1, Math.PI / 6);
@@ -16,7 +16,7 @@ export function marbleInit() {
     physics.addBody(world, slope2);
     const wheelWing1 = physics.createRectangleShape(world, { x: 200, y: 400 }, 10, 100);
     const wheelWing2 = physics.createRectangleShape(world, { x: 200, y: 400 }, 10, 100, Math.PI / 2);
-    const wheel = physics.createRigidBody(world, { x: 200, y: 400 }, 1, 0.3, 0.3, [wheelWing1, wheelWing2]);
+    const wheel = physics.createRigidBody(world, { x: 200, y: 400 }, 1, 0, 0.3, [wheelWing1, wheelWing2]);
     physics.addBody(world, wheel);
     const wheelJoint = physics.createCircle(world, { x: 200, y: 400 }, 10, 0, 0.3, 0.3, false);
     physics.addBody(world, wheelJoint);
